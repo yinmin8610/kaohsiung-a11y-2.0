957293b38be07e75acd15c9eb61cad89ad55a1ba
yinmin <alice0050722@gmail.com>
2020-09-14 14:00:48 +0800

    Update 2020-09-14T06:00:42.807Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -306,5 +306,32 @@ $.ajax({
       districtSelect.addEventListener('change', changeInfo, false);
     }
   }
+}); //swiper
+
+var swiper = new Swiper('.swiper-container', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    960: {
+      slidesPerView: 5
+    },
+    760: {
+      slidesPerView: 4
+    },
+    540: {
+      slidesPerView: 3
+    }
+  }
 });
 //# sourceMappingURL=all.js.map
