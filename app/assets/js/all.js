64cc1d37621e879c84af79d86e66229d56ad754b
yinmin <alice0050722@gmail.com>
2020-09-14 13:54:47 +0800

    輪播改輪播改swiper＋修正小錯誤

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -297,12 +297,30 @@ $.ajax({
   }
 })
 
-//輪播圖
-var computer = document.getElementById("carouselComputer");
-var phone = document.getElementById("carouselPhone");
-var width = document.body.clientWidth
-if (width < 575) {
-  phone.classList.add("active");
-} else { 
-  computer.classList.add("active");
+//swiper
+const swiper = new Swiper('.swiper-container', {
+  slidesPerView: 2,
+  spaceBetween: 30,
+  slidesPerGroup: 3,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    960:{
+      slidesPerView: 5
+    },
+    760: {
+        slidesPerView: 4
+    },
+    540: {
+        slidesPerView: 3
+    }
 }
+});
