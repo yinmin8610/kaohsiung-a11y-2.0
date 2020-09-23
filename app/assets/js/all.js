9b0740b3d7077c001fd99323600b377fe4fbdb44
hiYifang <a642630@gmail.com>
2020-09-23 22:34:44 +0800

    線上祭拜 JS 修改

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -12,7 +12,7 @@ if (btnStepOne) {
       name.classList.add("is-invalid");
       companyName.classList.add("is-invalid");
       address.classList.add("is-invalid");
-    } else if(name.value !== "" && companyName.value === "" && address.value === "") {
+    } else if (name.value !== "" && companyName.value === "" && address.value === "") {
       name.classList.add("is-valid");
       name.classList.remove("is-invalid");
       companyName.classList.add("is-invalid");
@@ -52,7 +52,7 @@ if (btnStepOne) {
       companyName.classList.remove("is-invalid");
       address.classList.add("is-valid");
       address.classList.remove("is-invalid");
-      
+
       var data = {
         "name": name.value,
         "companyName": companyName.value,
@@ -64,206 +64,49 @@ if (btnStepOne) {
   }
 }
 
-
-// 步驟二 toggleClass、push
-var apple = document.getElementById("apple");
-var orange = document.getElementById("orange");
-var banana = document.getElementById("banana");
-var grape = document.getElementById("grape");
-var threeAnimals = document.getElementById("threeAnimals");
-var flower = document.getElementById("flower");
+// 步驟二
+var arr = [];
 // 監聽 checkbox 點選事件
-if (apple) {
-  apple.checked == true;
-  apple.addEventListener('click', getChecked, false);
-}
-if (orange) {
-  orange.checked == true;
-  orange.addEventListener('click', getChecked, false);
-}
-if (banana) {
-  banana.checked == true;
-  banana.addEventListener('click', getChecked, false);
-}
-if (grape) {
-  grape.checked == true;
-  grape.addEventListener('click', getChecked, false);
-}
-if (threeAnimals) {
-  threeAnimals.checked == true;
-  threeAnimals.addEventListener('click', getChecked, false);
-}
-if (flower) {
-  flower.checked == true;
-  flower.addEventListener('click', getChecked, false);
-}
-function getChecked() {
-  if (apple.checked == true) {
-    document.querySelector(`[for='apple']`).classList.add("border");
-    document.querySelector(`[for='apple']`).classList.add("border-primary");
-    document.querySelector(`[for='apple']`).classList.add("bg-babyPrimary");
+$(document).on('change', '.checkbox', function () {
+  $(this).closest('label').toggleClass('border border-primary bg-babyPrimary');
+  var id = $(this).data("id");
+  if ($(this).is(":checked")) {
+    arr.push(id);
   } else {
-    document.querySelector(`[for='apple']`).classList.remove("border");
-    document.querySelector(`[for='apple']`).classList.remove("border-primary");
-    document.querySelector(`[for='apple']`).classList.remove("bg-babyPrimary");
+    var cache = arr.filter((item) => {
+      return item != id;
+    });
+    arr = cache
   }
+});
 
-  if (orange.checked == true) {
-    document.querySelector(`[for='orange']`).classList.add("border");
-    document.querySelector(`[for='orange']`).classList.add("border-primary");
-    document.querySelector(`[for='orange']`).classList.add("bg-babyPrimary");
-  } else {
-    document.querySelector(`[for='orange']`).classList.remove("border");
-    document.querySelector(`[for='orange']`).classList.remove("border-primary");
-    document.querySelector(`[for='orange']`).classList.remove("bg-babyPrimary");
-  }
-
-  if (banana.checked == true) {
-    document.querySelector(`[for='banana']`).classList.add("border");
-    document.querySelector(`[for='banana']`).classList.add("border-primary");
-    document.querySelector(`[for='banana']`).classList.add("bg-babyPrimary");
-  } else {
-    document.querySelector(`[for='banana']`).classList.remove("border");
-    document.querySelector(`[for='banana']`).classList.remove("border-primary");
-    document.querySelector(`[for='banana']`).classList.remove("bg-babyPrimary");
-  }
-
-  if (grape.checked == true) {
-    document.querySelector(`[for='grape']`).classList.add("border");
-    document.querySelector(`[for='grape']`).classList.add("border-primary");
-    document.querySelector(`[for='grape']`).classList.add("bg-babyPrimary");
-  } else {
-    document.querySelector(`[for='grape']`).classList.remove("border");
-    document.querySelector(`[for='grape']`).classList.remove("border-primary");
-    document.querySelector(`[for='grape']`).classList.remove("bg-babyPrimary");
-  }
-
-  if (threeAnimals.checked == true) {
-    document.querySelector(`[for='threeAnimals']`).classList.add("border");
-    document.querySelector(`[for='threeAnimals']`).classList.add("border-primary");
-    document.querySelector(`[for='threeAnimals']`).classList.add("bg-babyPrimary");
-  } else {
-    document.querySelector(`[for='threeAnimals']`).classList.remove("border");
-    document.querySelector(`[for='threeAnimals']`).classList.remove("border-primary");
-    document.querySelector(`[for='threeAnimals']`).classList.remove("bg-babyPrimary");
-  }
-
-  if (flower.checked == true) {
-    document.querySelector(`[for='flower']`).classList.add("border");
-    document.querySelector(`[for='flower']`).classList.add("border-primary");
-    document.querySelector(`[for='flower']`).classList.add("bg-babyPrimary");
-  } else {
-    document.querySelector(`[for='flower']`).classList.remove("border");
-    document.querySelector(`[for='flower']`).classList.remove("border-primary");
-    document.querySelector(`[for='flower']`).classList.remove("bg-babyPrimary");
-  }
-
-}
 // 監聽點選下一步事件
 var btnStepTwo = document.getElementById('stepTwo');
-var count = 0; // 計算被點選的祭品數目
 if (btnStepTwo) {
   btnStepTwo.addEventListener('click', getStepTwo, false);
   function getStepTwo(e) {
     e.preventDefault();
-    // 判斷是否為空值
-    if (apple.checked == true) {
-      count++;
-      localStorage.setItem('checkbox_apple', 'apple');
+    if (arr.length === 0) {
+      arr.push("apple");
+      localStorage.setItem('checkbox_object', JSON.stringify(arr));
+      document.location.href = "./worship.html";
     }
-    if (orange.checked == true) {
-      count++;
-      localStorage.setItem('checkbox_orange', 'orange');
+    if (arr.length > 0) {
+      localStorage.setItem('checkbox_object', JSON.stringify(arr));
+      document.location.href = "./worship.html";
     }
-    if (banana.checked == true) {
-      count++;
-      localStorage.setItem('checkbox_banana', 'banana');
-    }
-    if (grape.checked == true) {
-      count++;
-      localStorage.setItem('checkbox_grape', 'grape');
-    }
-    if (threeAnimals.checked == true) {
-      count++;
-      localStorage.setItem('checkbox_threeAnimals', 'threeAnimals');
-    }
-    if (flower.checked == true) {
-      count++;
-      localStorage.setItem('checkbox_flower', 'flower');
-    }
-    if (count === 0) {
-      // document.querySelector(`[for='apple']`).classList.add("border");
-      // document.querySelector(`[for='apple']`).classList.add("border-primary");
-      // document.querySelector(`[for='apple']`).classList.add("bg-babyPrimary");
-      // document.querySelector(`[for='orange']`).classList.add("border");
-      // document.querySelector(`[for='orange']`).classList.add("border-primary");
-      // document.querySelector(`[for='orange']`).classList.add("bg-babyPrimary");
-      // document.querySelector(`[for='banana']`).classList.add("border");
-      // document.querySelector(`[for='banana']`).classList.add("border-primary");
-      // document.querySelector(`[for='banana']`).classList.add("bg-babyPrimary");
-      // document.querySelector(`[for='grape']`).classList.add("border");
-      // document.querySelector(`[for='grape']`).classList.add("border-primary");
-      // document.querySelector(`[for='grape']`).classList.add("bg-babyPrimary");
-      // document.querySelector(`[for='threeAnimals']`).classList.add("border");
-      // document.querySelector(`[for='threeAnimals']`).classList.add("border-primary");
-      // document.querySelector(`[for='threeAnimals']`).classList.add("bg-babyPrimary");
-      // document.querySelector(`[for='flower']`).classList.add("border");
-      // document.querySelector(`[for='flower']`).classList.add("border-primary");
-      // document.querySelector(`[for='flower']`).classList.add("bg-babyPrimary");
-      // setTimeout(() => {
-      //   document.querySelector(`[for='apple']`).classList.remove("border");
-      //   document.querySelector(`[for='apple']`).classList.remove("border-primary");
-      //   document.querySelector(`[for='apple']`).classList.remove("bg-babyPrimary");
-      //   document.querySelector(`[for='orange']`).classList.remove("border");
-      //   document.querySelector(`[for='orange']`).classList.remove("border-primary");
-      //   document.querySelector(`[for='orange']`).classList.remove("bg-babyPrimary");
-      //   document.querySelector(`[for='banana']`).classList.remove("border");
-      //   document.querySelector(`[for='banana']`).classList.remove("border-primary");
-      //   document.querySelector(`[for='banana']`).classList.remove("bg-babyPrimary");
-      //   document.querySelector(`[for='grape']`).classList.remove("border");
-      //   document.querySelector(`[for='grape']`).classList.remove("border-primary");
-      //   document.querySelector(`[for='grape']`).classList.remove("bg-babyPrimary");
-      //   document.querySelector(`[for='threeAnimals']`).classList.remove("border");
-      //   document.querySelector(`[for='threeAnimals']`).classList.remove("border-primary");
-      //   document.querySelector(`[for='threeAnimals']`).classList.remove("bg-babyPrimary");
-      //   document.querySelector(`[for='flower']`).classList.remove("border");
-      //   document.querySelector(`[for='flower']`).classList.remove("border-primary");
-      //   document.querySelector(`[for='flower']`).classList.remove("bg-babyPrimary");
-      // }, 1000);
-      // alert('請選擇祭品');
-      return;
-    }
-    document.location.href = "./worship.html";
   }
 }
 
-
 // 步驟三
-var checkbox_apple = localStorage.getItem('checkbox_apple');
-var checkbox_orange = localStorage.getItem('checkbox_orange');
-var checkbox_banana = localStorage.getItem('checkbox_banana');
-var checkbox_grape = localStorage.getItem('checkbox_grape');
-var checkbox_threeAnimals = localStorage.getItem('checkbox_threeAnimals');
-var checkbox_flower = localStorage.getItem('checkbox_flower');
-// 判斷是否開啟圖片
-if (checkbox_apple === 'apple' && document.querySelector('#appleImg')) {
-  document.querySelector('#appleImg').classList.add("d-block");
+var getArr = JSON.parse(localStorage.getItem('checkbox_object'));
+// 顯示圖片
+if(location.pathname === '/worship.html'){
+ getArr.forEach(item => 
+  document.querySelector(`[data-img="${item}"]`).classList.add("d-block")
+ ); 
 }
-if (checkbox_orange === 'orange' && document.querySelector('#orangeImg')) {
-  document.querySelector('#orangeImg').classList.add("d-block");
-}
-if (checkbox_banana === 'banana' && document.querySelector('#bananaImg')) {
-  document.querySelector('#bananaImg').classList.add("d-block");
-}
-if (checkbox_grape === 'grape' && document.querySelector('#grapeImg')) {
-  document.querySelector('#grapeImg').classList.add("d-block");
-}
-if (checkbox_threeAnimals === 'threeAnimals' && document.querySelector('#threeAnimalsImg')) {
-  document.querySelector('#threeAnimalsImg').classList.add("d-block");
-}
-if (checkbox_flower === 'flower' && document.querySelector('#flowerImg')) {
-  document.querySelector('#flowerImg').classList.add("d-block");
-}
+
 // 監聽點選下一步事件
 var btnStepThree = document.getElementById('stepThree');
 if (btnStepThree) {
